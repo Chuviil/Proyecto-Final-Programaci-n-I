44e4d467562a6d018b65385272f9a5532a35e513
Sebastián Quito <70215395+Chuviil@users.noreply.github.com>
2022-06-22 22:40:08 +0000

    Añadida la función para Modificación de Usuarios

--- Readme.md
+++ Readme.md
@@ -6,12 +6,12 @@ Proyecto Final de Programación I UDLA
 ## Funcionalidades
 #### Usuario
 * Consulta de Saldo **✓**
-* Deposito de Saldo **NI**
+* Deposito de Saldo **✓**
 * Adquisición de Tickets **NI**
 #### Administrador
 * Registro de Usuarios **✓**
 * Eliminacion de Usuarios **NI**
-* Modificicacion de Usuarios **NI**
+* Modificicacion de Usuarios **✓**
 * Consulta de Usuarios **✓**
 ## Extra
 Libreria para creacion de codigos QR obtenida de: https://github.com/nayuki/QR-Code-generator
--- main.c
+++ main.c
@@ -5,29 +5,52 @@
 #include <ctype.h>
 #include "qrcodegen.h" //Libreria para generacion de codigos QR
 
+#define MAX_USUARIOS 100 //Cantidad Maxima de Usuarios
+#define MAX_NOMBRE_LEN 35 //Longitud Maxima de Nombre
 // Prototipos de las funciones del programa
 
 //Uso General
+
+//Funcion que formatea el nombre quitando espacios antes de ser guardado en el archivo de texto
 void FormatearNombrePIngreso(char[] /*NOMBRE*/);
-//^Funcion que formatea el nombre quitando espacios antes de ser guardado en el archivo de texto
+
+//Funcion que elimina el formato dado para uso dentro de la aplicacion
 void FormatearNombrePUso(char[]/*NOMBRE*/);
-//^Funcion que elimina el formato dado para uso dentro de la aplicacion
+
+//Elimina espacios de los nombres para el ingreso al archivo
 void QuitarEspacios(char[] /*NOMBRE*/);
-//^Elimina espacios de los nombres para el ingreso al archivo
+
 bool consultarUsuario(int /*CEDULA*/,char[] /*NOMBRE*/, float* /*SALDO*/);
+
+//Obtiene los usuarios del archivo "usuarios.txt" y los almacena dentro de los arreglos que se le pasan
+int obtenerUsuarios(char[MAX_USUARIOS][MAX_NOMBRE_LEN] /*NOMBRES*/, int[]/*CEDULAS*/, float[]/*SALDOS*/);
+
+//Obtiene el indice de un arreglo indicando donde esta dicho usuario
+int obtenerIndice(int /*CEDULA*/);
+
+//Actualiza el archivo "usuarios.txt" con la nueva informacion proporcionada
+void actualizarUsuarios(char[MAX_USUARIOS][MAX_NOMBRE_LEN]/*NOMBRES*/,int[]/*CEDULAS*/,float[]/*SALDOS*/);
 
 //Usuario
 
+//Modifica el saldo de el usuario ingresado por el valor ingresado
+void DepositarSaldo(int /*CEDULA*/);
 
 //Admin
+
+//Registra un Usuario con la informacion ortorgada
 void registrarUsuario(char[] /*NOMBRE*/, int /*CEDULA*/);
 // ^ Funcion de Ingreso de Datos
+
+//Modifica una de las variables(Nombre, Cedula o Saldo) de el usuario ingresado
+void modificarUsuario(int /*CEDULA*/);
 
 
 // Definicion de estructuras
 
+//Estructura de un usuario(Nombre, Cedula, Saldo)
 struct usuario{
-  char nombre[35];
+  char nombre[MAX_NOMBRE_LEN];
   int cedula;
   float saldo;
 };
@@ -43,9 +66,16 @@ static void printQr(const uint8_t qrcode[]);
 
 // Fin de los prototipos de las funciones para codigos QR
 
+//Variables Globales
+
+int cedulas[MAX_USUARIOS];
+char nombres[MAX_USUARIOS][MAX_NOMBRE_LEN];
+float saldos[MAX_USUARIOS];
+int cantUsuarios;
+struct usuario usuarioGeneral;
+
 int main(void) 
 {
-  struct usuario usuarioGeneral;
   int opcion;
   char mensaje[100];
   do
@@ -64,9 +94,19 @@ int main(void)
           switch(opcion)
           {
             case 1:
-              system("clear");
-              printf("Ingrese su numero de cedula: ");
-              scanf("%d", &usuarioGeneral.cedula);
+              do
+              {
+                system("clear");
+                printf("Ingrese su numero de cedula: ");
+                scanf("%d", &usuarioGeneral.cedula);
+                if(!(usuarioGeneral.cedula>0))
+                {
+                  system("clear");
+                  printf("\033[1;31mCedula Invalida!\033[0m");
+                  fflush(stdout);
+                  sleep(1);
+                }
+              }while(!(usuarioGeneral.cedula>0));
               if(consultarUsuario(usuarioGeneral.cedula,usuarioGeneral.nombre,&usuarioGeneral.saldo))
               {
                 printf("Usuario Encontrado!\n");
@@ -75,14 +115,27 @@ int main(void)
               }
               else
               {
-                printf("Usuario No Encontrado!");
+                printf("\033[1;31mUsuario No Encontrado!\033[0m");
               }
               getchar();
               getchar();
               system("clear");
             break;
             case 2:
-              
+              do
+              {
+                system("clear");
+                printf("Ingrese su numero de cedula: ");
+                scanf("%d", &usuarioGeneral.cedula);
+                if(!(usuarioGeneral.cedula>0))
+                {
+                  system("clear");
+                  printf("\033[1;31mCedula Invalida!\033[0m");
+                  fflush(stdout);
+                  sleep(1);
+                }
+              }while(!(usuarioGeneral.cedula>0));
+              DepositarSaldo(usuarioGeneral.cedula);
             break;
             case 3:
               
@@ -119,7 +172,20 @@ int main(void)
               
             break;
             case 3:
-              
+              do
+              {
+                system("clear");
+                printf("Ingrese el numero de cedula del usuario a modificar: ");
+                scanf("%d", &usuarioGeneral.cedula);
+                if(!(usuarioGeneral.cedula>0))
+                {
+                  system("clear");
+                  printf("\033[1;31mCedula Invalida!\033[0m");
+                  fflush(stdout);
+                  sleep(1);
+                }
+              }while(!(usuarioGeneral.cedula>0));
+              modificarUsuario(usuarioGeneral.cedula);
             break;
             case 4:
               system("clear");
@@ -182,6 +248,45 @@ int main(void)
 
 // Declaracion de funciones del programa
 
+void DepositarSaldo(int cedula)
+{
+  int indice, opcion;
+  float deposito;
+  if(consultarUsuario(cedula, usuarioGeneral.nombre, &usuarioGeneral.saldo))
+  {
+    obtenerUsuarios(nombres, cedulas, saldos);
+    indice = obtenerIndice(cedula);
+    printf("Usuario Encontrado\n\nNombre: %s\nCedula: %d\nSaldo: %.2f$\nDesea continuar?\n1)Si\t2)No\nElija una opcion: ",nombres[indice],cedulas[indice],saldos[indice]);
+    scanf("%d",&opcion);
+    system("clear");
+    switch(opcion)
+    {
+      case 1:
+        printf("Ingrese cuanto saldo desea depositar: ");
+        scanf("%f",&deposito);
+        system("clear");
+        printf("Gracias por su deposito\n\nNombre: %s\nCedula: %d\nSaldo anterior: %.2f$\nNuevo Saldo: %.2f$ ",nombres[indice],cedulas[indice],saldos[indice],saldos[indice]+deposito);
+        saldos[indice] += deposito;
+        actualizarUsuarios(nombres,cedulas,saldos);
+      break;
+      case 2:
+        printf("Regresando...");
+      break;
+      default:
+        printf("Opcion Invalida");
+      break;
+    }
+  }
+  else
+  {
+    printf("Usuario No Encontrado!");
+  }
+  fflush(stdout);
+  getchar();
+  getchar();
+  system("clear");
+}
+
 void registrarUsuario(char nombre[], int cedula)
 {
   int opcion;
@@ -189,7 +294,7 @@ void registrarUsuario(char nombre[], int cedula)
   FILE *puntero;
   printf("===========================\n\tRegistro de Usuario\n===========================\n\n");
   printf("Ingrese el nombre: ");
-  fgets(nombre,35,stdin);
+  fgets(nombre,MAX_NOMBRE_LEN,stdin);
   nombre[strcspn(nombre,"\n")] = 0;
   do
   {
@@ -280,7 +385,7 @@ bool consultarUsuario(int cedula, char nombre[], float *saldo)
   do
   {
     finArchivo = feof(puntero);
-    memset(nombre,0,35);
+    memset(nombre,0,MAX_NOMBRE_LEN);
     fscanf(puntero,"%d %s %f", &cedulatemp, nombre, saldo);
     if(cedulatemp == cedula)
     {
@@ -322,6 +427,161 @@ void FormatearNombrePUso(char nombre[])
   }
 }
 
+int obtenerUsuarios(char nombres[MAX_USUARIOS][MAX_NOMBRE_LEN], int cedulas[], float saldos[])
+{
+  memset(nombres,0,MAX_USUARIOS);
+  memset(cedulas,0,MAX_USUARIOS);
+  memset(saldos,0,MAX_USUARIOS);
+  FILE *puntero;
+  int index = 0; 
+  cantUsuarios = 0;
+  puntero = fopen("datos/usuarios.txt", "r");
+  while(!feof(puntero))
+  {
+    fscanf(puntero, "%d %s %f", &cedulas[index], nombres[index], &saldos[index]);
+    FormatearNombrePUso(nombres[index]);
+    index++;
+    cantUsuarios++;
+  }
+  fclose(puntero);
+  cantUsuarios--;
+  return cantUsuarios;
+}
+
+int obtenerIndice(int cedula)
+{
+  int indice;
+  for(int i = 0;i < sizeof(cedulas);i++)
+  {
+    if(cedulas[i]==cedula)
+    {
+      indice = i;
+      break;
+    }
+  }
+  return indice;
+}
+
+void actualizarUsuarios(char nombres[MAX_USUARIOS][MAX_NOMBRE_LEN], int cedulas[], float saldos[])
+{
+  FILE *puntero;
+  puntero = fopen("datos/usuarios.txt","w");
+  for(int i = 0;i < cantUsuarios; i++)
+  {
+    FormatearNombrePIngreso(nombres[i]);
+    fprintf(puntero,"%d %s %.2f\n", cedulas[i], nombres[i], saldos[i]);
+  }
+  fclose(puntero);
+}
+
+void modificarUsuario(int cedula)
+{
+  struct usuario UsuarioTemp;
+  int indice,opcion,modOpcion;
+  system("clear");
+  if(consultarUsuario(cedula, usuarioGeneral.nombre, &usuarioGeneral.saldo))
+  {
+    obtenerUsuarios(nombres, cedulas, saldos);
+    indice = obtenerIndice(cedula);
+    do
+    {
+      system("clear");
+      printf("============================\n\tMODIFICACION USUARIO\n============================\n\nNombre: %s\nCedula: %d\nSaldo: %.2f$\n\nQue desea modificar?\n1)Nombre\n2)Cedula\n3)Saldo\n4)Salir\nElija una opcion: ",nombres[indice],cedulas[indice],saldos[indice]);
+      scanf("%d",&opcion);
+      switch(opcion)
+      {
+        case 1:
+          system("clear");
+          printf("==========================\n\tModificando Nombre\n==========================\n\nIngrese el nuevo nombre: ");
+          getchar();
+          fgets(UsuarioTemp.nombre,MAX_NOMBRE_LEN,stdin);
+          UsuarioTemp.nombre[strcspn(UsuarioTemp.nombre,"\n")] = 0;
+          system("clear");
+          do
+          {
+            printf("==========================\n\tModificando Nombre\n==========================\n\nAnterior Nombre: %s\nNuevo Nombre: %s\n\nDesea realizar el cambio?\n1)Si\t2)No\nElija una opcion: ",nombres[indice],UsuarioTemp.nombre);
+            scanf("%d",&modOpcion);
+            switch(modOpcion)
+            {
+              case 1:
+                FormatearNombrePIngreso(UsuarioTemp.nombre);
+                FormatearNombrePUso(UsuarioTemp.nombre);
+                strcpy(nombres[indice],UsuarioTemp.nombre);
+                actualizarUsuarios(nombres,cedulas,saldos);
+                FormatearNombrePUso(nombres[indice]);
+              break;
+              case 2:
+              break;
+              default:
+                system("clear");
+                printf("\n\033[1;31mOpcion Invalida intente de nuevo.\n\n\033[0m");
+              break;
+            }
+          }while(!(modOpcion>0&&modOpcion<3));
+        break;
+        case 2:
+          system("clear");
+          printf("==========================\n\tModificando Cedula\n==========================\n\nIngrese la nueva cedula: ");
+          scanf("%d", &UsuarioTemp.cedula);
+          system("clear");
+          do
+          {
+            printf("==========================\n\tModificando Cedula\n==========================\n\nAnterior Cedula: %d\nNueva Cedula: %d\n\nDesea realizar el cambio?\n1)Si\t2)No\nElija una opcion: ",cedulas[indice], UsuarioTemp.cedula);
+            scanf("%d",&modOpcion);
+            switch(modOpcion)
+            {
+              case 1:
+                cedulas[indice] = UsuarioTemp.cedula;
+                actualizarUsuarios(nombres,cedulas,saldos);
+              break;
+              case 2:
+              break;
+              default:
+                system("clear");
+                printf("\n\033[1;31mOpcion Invalida intente de nuevo.\n\n\033[0m");
+              break;
+            }
+          }while(!(modOpcion>0&&modOpcion<3));
+        break;
+        case 3:
+          system("clear");
+          printf("==========================\n\tModificando Saldo\n==========================\n\nIngrese el nuevo saldo: ");
+          scanf("%f", &UsuarioTemp.saldo);
+          system("clear");
+          do
+          {
+            printf("==========================\n\tModificando Saldo\n==========================\n\nAnterior Saldo: %.2f$\nNuevo Saldo: %.2f$\n\nDesea realizar el cambio?\n1)Si\t2)No\nElija una opcion: ",saldos[indice], UsuarioTemp.saldo);
+            scanf("%d",&modOpcion);
+            switch(modOpcion)
+            {
+              case 1:
+                saldos[indice] = UsuarioTemp.saldo;
+                actualizarUsuarios(nombres,cedulas,saldos);
+              break;
+              case 2:
+              break;
+              default:
+                system("clear");
+                printf("\n\033[1;31mOpcion Invalida intente de nuevo.\n\n\033[0m");
+              break;
+            }
+          }while(!(modOpcion>0&&modOpcion<3));
+        break;
+        case 4:
+          sleep(1);
+          system("clear");
+        break;
+        default:
+          printf("\n\033[1;31mOpcion Invalida intente de nuevo.\n\n\033[0m");
+        break;
+      }
+    }while(opcion!=4);
+  }
+  else
+  {
+    
+  }
+}
 // Fin de Declaracion de funciones del programa
 
 // Funciones de la generacion de codigos QR
